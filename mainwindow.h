90d84c02f3c555dbb0c36087dd7bb4682a80e54b
jgcodes2020 <jackyguo18@hotmail.com>
2021-10-19 08:47:54 -0400

    Finish resolving merge

--- mainwindow.h
+++ mainwindow.h
@@ -2,12 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-<<<<<<< HEAD
-#include <qevent.h>
-=======
 #include <QMouseEvent>
 #include <QPoint>
->>>>>>> c889c926649ede570ac315bb57a5c62ecef83df3
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
